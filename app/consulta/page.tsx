1317baf0d034fd0369986d16976f978e82d406c7
ElisaaMartins <liaparecidamartinsoliveira@gmail.com>
2023-10-24 00:31:32 -0300

    TCCversão24_10

--- app/consulta/page.tsx
+++ app/consulta/page.tsx
@@ -5,14 +5,14 @@ import Image from 'next/image'
 import lupa from 'images/lupa.png'
 import 'app/consulta/components/consultas.css'
 
-export default function Consultas() {
+export default function consulta() {
   const [ecadastro, setecadastro] = useState(false)
   const googleLogoUrl = "https://cdn-icons-png.flaticon.com/512/281/281764.png?w=740&t=st=1691100843~exp=1691101443~hmac=a30f55d5ff66b960de01a09d3cc7882cd6fd49341fdc97cfb099ed6a7bcde8a9"
 
-  const alternapagina = () => {
+/*   const alternapagina = () => {
     const novoestado = !ecadastro;
     setecadastro(novoestado)
-  }
+  } */
 
   return (
     <main>
