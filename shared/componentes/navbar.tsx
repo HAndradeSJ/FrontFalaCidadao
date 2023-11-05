266b8da5ed786340b3f5d4326faefeb7cb38cd01
ElisaaMartins <liaparecidamartinsoliveira@gmail.com>
2023-11-04 22:52:45 -0300

    navbar c/ hover

--- shared/componentes/navbar.tsx
+++ shared/componentes/navbar.tsx
@@ -1,29 +1,54 @@
 'use client'
 import 'node_modules/bootstrap/dist/css/bootstrap.min.css'
 import React, { useEffect } from 'react'
+import Image from 'next/image';
+import logo from 'images/logo.png'
+import '../componentes/navbar.css'
+import Link from 'next/link';
 
-export function Navbar () {
+
+export function Navbar() {
 
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
-    return (
-<nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor:"#162E98"}}>
-  <div className="container-fluid">
-    <a className="navbar-brand" href="./">Fala Cidadão</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="./">Home</a>
-        <a className="nav-link active" aria-current="page" href="./login">Login</a>
-        <a className="nav-link active" aria-current="page" href="./about">Sobre</a>
-        <a className="nav-link active" aria-current="page" href="./contato">Fale Conosco</a>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#162E98' }}>
+      <div className="container-fluid">
+        <Image id="perfil" src={logo} alt="logo" style={{ width: '3rem', height: '3rem', marginLeft: '3rem' }} />
+        <a className="navbar-brand" href="./" id='falaCidadao'>
+          Fala Cidadão
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        <div className="collapse navbar-collapse">
+          <div className="navbar-nav" id='divNav'>
+            <a className="nav-link active navbar-hover" aria-current="page" href="./" style={{ marginLeft: '5rem' }}>
+              Home
+            </a>
+            <a className="nav-link active navbar-hover" aria-current="page" href="./login" style={{ marginLeft: '5rem' }}>
+              Login
+            </a>
+            <a className="nav-link active navbar-hover" aria-current="page" href="./about" style={{ marginLeft: '5rem' }}>
+              Sobre
+            </a>
+            <a className="nav-link active navbar-hover" aria-current="page" href="./contato" style={{ marginLeft: '5rem' }}>
+              Fale Conosco
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</nav>
-    )
+    </nav>
+  );
 }
